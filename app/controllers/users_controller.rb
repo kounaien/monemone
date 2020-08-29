88b1452d053c7f97ac2d4f94dd43c0a12a5ba93e
kounaien <1127fujihiroto@gmail.com>
2020-08-29 17:35:44 +0900

    fix chart.js

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,9 @@ class UsersController < ApplicationController
         @post_comment = PostComment.new
         @posts = @user.posts.all
         if @posts.any?
-            @count_sum = @user.posts.data_format.for_date_range(@user.posts.first.created_at, @user.posts.last.created_at).count_sum
-
+            @count_sum = @user.posts.for_date_range(@user.posts.first.created_at, @user.posts.last.created_at).count_sum
+            @count_sum.transform_keys { |key| key.to_s }
+            
             total = 0
             @count_sum.each do |key, value|
                 total += value
