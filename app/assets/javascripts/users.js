b1cd7d82ec256fca447578641adaa255cdb6e6e0
kounaien <1127fujihiroto@gmail.com>
2020-08-31 01:23:01 +0900

    chart.js remove decimal point

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -4,17 +4,26 @@ $(document).on('turbolinks:load', function() {
     $('#nav-tab').on('click', function() {
         var ctx = document.getElementById('myChart'); 
         var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: JSON.parse(ctx.dataset.label),
-            datasets: [{
-                label: '円',
-                data:JSON.parse(ctx.dataset.data),
-                backgroundColor: "rgba(10, 100, 10, 0.1)",
-                borderColor: "rgba(10, 150, 10, 1)",
-                borderWidth: 1
-            }]
-        },
+            type: 'line',
+            data: {
+                labels: JSON.parse(ctx.dataset.label),
+                datasets: [{
+                    label: '円',
+                    data:JSON.parse(ctx.dataset.data),
+                    backgroundColor: "rgba(10, 100, 10, 0.1)",
+                    borderColor: "rgba(10, 150, 10, 1)",
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            callback: function(value) {return ((value % 10) == 0)? value : ''}
+                        }
+                    }],
+                },
+            }
         });
     });
 });
